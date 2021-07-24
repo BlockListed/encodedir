f29189e68e1e1927a54a3d30ecd015f3be40804c
BlockListed <github@aaronlowitzsch.com>
2021-07-24 19:58:09 +0200

    Made sure License notices don't show up in line counts by prepending every line with a /

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 /*
-Encodedir: Encode all video files in a directory using the systems installed ffmpeg
-Copyright (C) 2021  BlockListed
+/Encodedir: Encode all video files in a directory using the systems installed ffmpeg
+/Copyright (C) 2021  BlockListed
+/
+/This program is free software: you can redistribute it and/or modify
+/it under the terms of the GNU General Public License as published by
+/the Free Software Foundation, either version 3 of the License, or
+/(at your option) any later version.
+/
+/This program is distributed in the hope that it will be useful,
+/but WITHOUT ANY WARRANTY; without even the implied warranty of
+/MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/GNU General Public License for more details.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/You should have received a copy of the GNU General Public License
+/along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 mod read_cfg;
@@ -23,6 +23,7 @@ mod encode;
 fn main() {
     /*
         TODO:
+        * GPL stuff: https://www.gnu.org/licenses/gpl-3.0.html#howto
         * Add GPL Warning:
 <program>  Copyright (C) <year>  <name of author>
 This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
